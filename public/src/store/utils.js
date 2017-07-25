e796bcfcd04ab598de05ea2a661e14ec54043035
Dimitry Leonov <dimitry.leonov@trustyou.net>
2017-07-25 10:52:06 +0200

    Autobuild host from enviroment

--- public/src/store/utils.js
+++ public/src/store/utils.js
@@ -1,6 +1,6 @@
 import request from 'superagent'
 
-const HOST = 'http://localhost:3001'
+const HOST = `${window.location.protocol}//${window.location.hostname}:3001`
 const buildUrl = (u, b) => HOST + u + (b ? '/' + b : '')
 
 const makeRequest = (url, bundle=null, data={}, method='get') => {
